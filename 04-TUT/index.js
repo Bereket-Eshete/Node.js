34337148afa47199d60a3fafc16293b749f3e013
Bereket-Eshete <bereketeshete63@gmail.com>
2025-07-25 22:10:26 +0300

    complete chapter 4: Event Emitter

---
+++ 04-TUT/index.js
@@ -0,0 +1,14 @@
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+class MyEmitter extends EventEmitter {}
+
+// Initialize Object (renamed to avoid conflict)
+const myEmitter = new MyEmitter();
+
+// Add Listener for log event
+myEmitter.on("log", (message) => logEvents(message));
+
+setTimeout(() => {
+  myEmitter.emit("log", "Log event emitted!");
+}, 2000);
---
+++ 04-TUT/logEvents.js
@@ -0,0 +1,29 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const fspromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), "yyyy-MM-dd HH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fspromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fspromises.appendFile(
+      path.join(__dirname, "logs", "EventLog.txt"),
+      logItem
+    );
+  } catch (err) {
+    console.err(err);
+  }
+};
+
+console.log("HELLO");
+console.log("Current DateTime:", format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+console.log("Random UUID:", uuid());
+
+module.exports = logEvents;
